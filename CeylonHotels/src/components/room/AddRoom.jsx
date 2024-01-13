3a0cdd36748078fd1c0c9c3815a28e84335a3ee6
Saranga Nayanajith <karunasundara_kmsn_e21@engug.ruh.ac.lk>
2024-01-14 01:34:47 +0530

    AddRoom Component Initiaze

---
+++ CeylonHotels/src/components/room/AddRoom.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const AddRoom = () => {
+    const [newRoom, setnewRoom] = useState({
+        photo: null,
+        roomType: "",
+        roomPrice: "",
+    });
+    const [imagePreview, setimagePreview] = useState("");
+    const [sucessMessage, setsucessMessage] = useState("");
+    const [errorMessage, seterrorMessage] = useState("");
+    
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default AddRoom
